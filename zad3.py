1655e9efbd2daeeb658f52430408e72f5985994e
kappa243 <edek15pl@gmail.com>
2020-10-28 16:19:27 +0100

    zad3

--- zad3.py
+++ zad3.py
@@ -1,6 +1,5 @@
 def sito(n):
     tab = [True] * (n - 1)
-    count = 0
 
     for i in range(2, int(n ** 0.5) + 1):
         if tab[i - 2]:
@@ -8,17 +7,14 @@ def sito(n):
             while j < n - 1:
                 if tab[j]:
                     tab[j] = False
-                    count += 1
                 j += i
 
+    output = []
     for i in range(2, n + 1):
         if tab[i - 2]:
-            tab[i - 2] = i
+            output.append(i)
 
-    for i in range(count):
-        tab.remove(False)
-
-    print(tab)
+    print(output)
 
 
-sito(25)
+sito(100)
